4ccfe0af9301bc6eb821cd475b1eab060e084813
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-07-22 21:38:45 -0300

    included new exercise

--- exercises/idade_exercise.js
+++ exercises/idade_exercise.js
@@ -4,4 +4,5 @@ var nome = prompt("Digite seu nome: ");
 var idade = parseInt(prompt("Digite sua idade: "));
 
 var maior_idade = idade >= 18
-console.log(maior_idade)
+console.log(maior_idade) // printa na tela
+console.log(typeof maior_idade) // tipo da variável
