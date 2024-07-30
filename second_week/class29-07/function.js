6fa71a7ca222cfe1cc004330cdad73768c31cea3
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-07-29 21:06:07 -0300

    add files js,css and html

---
+++ second_week/class29-07/function.js
@@ -0,0 +1,11 @@
+function calcularMediaNotas(notaUm, notaDois, notaTres, notaQuatro) {
+    var mediaNotas = (notaUm + notaDois + notaTres + notaQuatro) / 4
+    return mediaNotas
+}
+
+function olaMundo() {
+    console.log("Ola mundo")
+}
+
+var resultado = calcularMediaNotas(9, 10, 4, 7)
+console.log(calcularMediaNotas(9, 10, 4, 7))
