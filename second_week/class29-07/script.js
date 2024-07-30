d7459ba7755440f44ef6474a5cbd98c57ea6412e
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-07-29 22:00:03 -0300

    [henrique] add events in javascript

--- second_week/class29-07/script.js
+++ second_week/class29-07/script.js
@@ -2,4 +2,14 @@ function calcularMediaNotas(notaUm, notaDois, notaTres, notaQuatro) {
     var mediaNotas = (notaUm + notaDois + notaTres + notaQuatro) / 4
     return mediaNotas
 }
+//get the class
+// document.getElementsByClassName('container') 
 
+// get the id
+document.getElementById("calculator").addEventListener("submit", function(event) {
+    event.preventDefault
+    let nota1 = document.getElementById("nota1").value
+    let nota2 = document.getElementById("nota2").value
+    let nota3 = document.getElementById("nota3").value
+    let nota4 = document.getElementById("nota4").value
+})
