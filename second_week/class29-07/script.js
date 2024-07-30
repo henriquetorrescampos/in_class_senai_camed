40f94538807eafd993d02ad58ded08603f7f5841
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-07-30 19:34:53 -0300

    add new js files

--- second_week/class29-07/script.js
+++ second_week/class29-07/script.js
@@ -1,15 +1,25 @@
 function calcularMediaNotas(notaUm, notaDois, notaTres, notaQuatro) {
-    var mediaNotas = (notaUm + notaDois + notaTres + notaQuatro) / 4
-    return mediaNotas
+  var mediaNotas = (notaUm + notaDois + notaTres + notaQuatro) / 4;
+  return mediaNotas;
 }
 //get the class
-// document.getElementsByClassName('container') 
-
+// document.getElementsByClassName('class')
 // get the id
-document.getElementById("calculator").addEventListener("submit", function(event) {
-    event.preventDefault()
-    let nota1 = document.getElementById("nota1").value
-    let nota2 = document.getElementById("nota2").value
-    let nota3 = document.getElementById("nota3").value
-    let nota4 = document.getElementById("nota4").value
-})
+//document.getElementById('id')
+
+function executarFormulario(event) {
+
+  event.preventDefault()
+
+  let nota1 = document.getElementById("nota1").value;
+  let nota2 = document.getElementById("nota2").value;
+  let nota3 = document.getElementById("nota3").value;
+  let nota4 = document.getElementById("nota4").value; 
+
+  calcularMediaNotas(nota, nota2, nota3, nota4)
+}
+
+
+document //document arquivo que recebe script
+  .getElementById('calculator') //pega o elemento by id
+  .addEventListener('submit', executarFormulario)
