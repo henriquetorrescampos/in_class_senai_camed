3c7b88e88fc3ce60be8868d2fc3c0ee3eb265966
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-07-30 19:55:13 -0300

    add parseFloat in notas

--- second_week/class29-07/script.js
+++ second_week/class29-07/script.js
@@ -1,25 +1,24 @@
 function calcularMediaNotas(notaUm, notaDois, notaTres, notaQuatro) {
-  var mediaNotas = (notaUm + notaDois + notaTres + notaQuatro) / 4;
+  let mediaNotas = (notaUm + notaDois + notaTres + notaQuatro) / 4;
   return mediaNotas;
 }
+
 //get the class
 // document.getElementsByClassName('class')
 // get the id
 //document.getElementById('id')
 
 function executarFormulario(event) {
+  event.preventDefault();
 
-  event.preventDefault()
+  let nota1 = parseFloat(document.getElementById("nota1").value);
+  let nota2 = parseFloat(document.getElementById("nota2").value);
+  let nota3 = parseFloat(document.getElementById("nota3").value);
+  let nota4 = parseFloat(document.getElementById("nota4").value);
 
-  let nota1 = document.getElementById("nota1").value;
-  let nota2 = document.getElementById("nota2").value;
-  let nota3 = document.getElementById("nota3").value;
-  let nota4 = document.getElementById("nota4").value; 
-
-  calcularMediaNotas(nota, nota2, nota3, nota4)
+  alert(calcularMediaNotas(nota1, nota2, nota3, nota4).toFixed(2));
 }
 
-
 document //document arquivo que recebe script
-  .getElementById('calculator') //pega o elemento by id
-  .addEventListener('submit', executarFormulario)
+  .getElementById("calculator") //pega o elemento by id
+  .addEventListener("submit", executarFormulario);
