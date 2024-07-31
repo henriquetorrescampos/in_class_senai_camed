d01a3ae64d4ca6a3db81b0d2b9b6c776e6a69ea4
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-07-30 21:22:24 -0300

    .

--- second_week/class30-07/formules_script.js
+++ second_week/class30-07/formules_script.js
@@ -1,3 +1,20 @@
-function (params) {
-  
+function calcularPerimetroTriangulo(ladoA, ladoB, ladoC) {
+  let perimetro = ladoA + ladoB + ladoC;
+  return perimetro;
 }
+
+function executarForm(event) {
+  event.preventDefault();
+
+  let ladoA = parseFloat(document.getElementById("ladoA").value);
+  let ladoB = parseFloat(document.getElementById("ladoB").value);
+  let ladoC = parseFloat(document.getElementById("ladoC").value);
+
+  let resultado = calcularPerimetroTriangulo(ladoA, ladoB, ladoC);
+
+  document.getElementById("result").innerText = `O resultado é ${resultado}.`;
+}
+
+document
+  .getElementById("form-perimetro-triangulo")
+  .addEventListener("submit", executarForm);
